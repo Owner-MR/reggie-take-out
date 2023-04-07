3a7b85ab07acfb1fbe749b0c4fd94dcb68150d6a
mr <123456@qq.com>
2023-04-07 20:19:31 +0800

    环境搭建

---
+++ src/main/java/com/mr/reggie/mapper/EmployeeMapper.java
@@ -0,0 +1,8 @@
+package com.mr.reggie.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mr.reggie.entity.Employee;
+
+public interface EmployeeMapper extends BaseMapper<Employee>{
+
+}
