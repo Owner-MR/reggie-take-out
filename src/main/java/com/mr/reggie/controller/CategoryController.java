a48d603e4cb51284503799bdd25e25c142bed029
mr <2224769764@qq.com>
2023-04-12 23:36:27 +0800

    菜品分类

--- src/main/java/com/mr/reggie/controller/CategoryController.java
+++ src/main/java/com/mr/reggie/controller/CategoryController.java
@@ -1,12 +1,15 @@
 package com.mr.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.mr.reggie.common.R;
+import com.mr.reggie.entity.Category;
 import com.mr.reggie.service.CategoryService;
 import com.mr.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -15,4 +18,23 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    /**
+     * 新增分类
+     * @param category
+     * @return
+     */
+    @PostMapping
+    public R<String> save(@RequestBody Category category){
+        log.info("category:{}", category);
+        categoryService.save(category);
+        return R.success("新增分类成功");
+    }
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize){
+        Page<Category> pageInfo = new Page<>(page, pageSize);
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.orderByAsc(Category::getSort);
+        categoryService.page(pageInfo, queryWrapper);
+        return R.success(pageInfo);
+    }
 }
